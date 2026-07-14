3781058770f3e954a7bcd862f02a5e17bf104719
Victorious <your-email@example.com>
2026-07-14 19:57:13 +0300

    Add backend home route

--- server.js
+++ server.js
@@ -122,6 +122,9 @@ const PORT = process.env.PORT || 5000;
 server.listen(3000, () => {
     console.log("⚡ ChargeFlow running with PostgreSQL");
 });
+app.get("/", (req, res) => {
+    res.send("EV Weather Backend is running successfully");
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
